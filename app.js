66c0685786ad55db47b5fe46a3cbefcaff7b310a
prashantdudz <86475054+prashantdudz@users.noreply.github.com>
2021-06-25 23:05:05 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to new world of codes'));
+app.get('/', (req, res) => res.send('Welcome to new world of pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
